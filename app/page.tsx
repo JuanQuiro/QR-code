511b0bf58cbc7efb6f323c011aafcb238ae79b17
juanquiro <juanquirozsanchez@hotmail.com>
2023-03-27 03:11:05 +0000

    32

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main >
       <section className='grid shadow-xl p-3 bg-white rounded-3xl' >
         <Image height={300} width={300} className='rounded-3xl' src={qrr} alt="" />
-        <div className='w-[18.75rem] mt-5 px-5'>
+        <div className='w-[18.75rem]  mt-5 px-5'>
           <h1 className={`${inter.className} text-xl text-center font-bold`}>Improve your front-end skill by building projects</h1>
           <p className='text-center font-semibold opacity-40 my-5'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
