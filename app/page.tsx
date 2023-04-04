19b26dcaef2f291b4b1e8b7423b618cf8b4cb6a3
juanquiro <juanquirozsanchez@hotmail.com>
2023-04-04 18:32:19 +0000

    v3

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main >
-      <section className='grid shadow-xl p-3 bg-white rounded-3xl' >
-        <Image height={300} width={300} className='rounded-3xl' src={qrr} alt="Qr de ejemplo" />
+      <section className='grid shadow-xl shadow-gray-400 p-3 bg-white rounded-3xl' >
+        <Image height={300} width={300} className='rounded-[2rem]' src={qrr} alt="Qr de ejemplo" />
         <div className='w-[18.75rem]  mt-5 px-5'>
           <h1 className={`${inter.className} text-black/70 text-xl text-center font-black`}>Improve your front-end skill by building projects</h1>
-          <p className='leading-tight	 text-center font-semibold opacity-40 my-5'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          <p className='text-xl leading-tight	 text-center font-[650] opacity-40 my-5'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </section>
 
